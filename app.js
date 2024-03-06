ae112d8d2aef4483148d4323c393535560dafa04
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-03-06 21:58:39 +0300

    feat: resolve task 1-map

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const array = [
+  {
+    id: 1,
+    name: "Вася",
+  },
+  {
+    id: 2,
+    name: "Петя",
+  },
+  {
+    id: 1,
+    name: "Вася",
+  },
+];
+
+const set = new Set();
+
+const find = array.find((el) => {
+  return el.id === 1;
+});
+
+const map = array.map((el) => {
+  if (find !== el) {
+    set.add(el);
+  }
+});
+
+console.log(set);
