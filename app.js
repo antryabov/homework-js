13171875ce62b99ce93fd85c415d10760544ff17
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-03-10 13:44:17 +0300

    feat: solve 3-birthday task

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const user = {
+  name: "Vasia",
+  birthday: "03/10/2010",
+};
+
+function validateAge(user) {
+  const dateBirhday = new Date(user.birthday);
+  const now = new Date();
+  const age = now.getFullYear() - dateBirhday.getFullYear();
+  console.log(age);
+  if (age > 14) {
+    return true;
+  }
+  if (age === 14) {
+    if (dateBirhday.getMonth() >= now.getMonth()) {
+      if (dateBirhday.getDate() >= now.getDate()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(validateAge(user));
