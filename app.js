a4e46c3a3f740cce35842442cfd9680b20fd3033
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-03-10 19:56:33 +0300

    bug: create function for timer pizza, but there are bugs

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function timerPizza(time) {
+  const timer = new Date();
+  let milliseconds = timer.getTime() - (timer.getTime() - time * 1000);
+  let seconds = (milliseconds / 1000) % 60;
+  let minute = (seconds / 60) % 60;
+  console.log(minute);
+  const intervalSecond = setInterval(() => {
+    console.log(`0${minute}:0${++seconds}`);
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(intervalSecond);
+  }, time * 1000);
+}
+
+console.log(timerPizza(120));
