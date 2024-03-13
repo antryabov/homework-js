9839462917e2b3c243f4de7e291c86ca63b7df01
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-03-13 19:27:59 +0300

    feat: solve task

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 "use strict";
 
-function timerPizza(time) {
-  const timer = new Date();
-  let milliseconds = timer.getTime() - (timer.getTime() - time * 1000);
-  let seconds = (milliseconds / 1000) % 60;
-  let minute = (seconds / 60) % 60;
-  console.log(minute);
-  const intervalSecond = setInterval(() => {
-    console.log(`0${minute}:0${++seconds}`);
+const timer = document.querySelector(".timer");
+
+function isNewYear() {
+  const end = new Date(2025, 0, 1, 0, 0, 0).getTime();
+  const interval = setInterval(() => {
+    const untilTheNY = new Date(end - new Date() - 10800000);
+    let month = untilTheNY.getMonth();
+    let days = untilTheNY.getDate();
+    let hour = untilTheNY.getHours();
+    let minute = untilTheNY.getMinutes();
+    let seconds = untilTheNY.getSeconds();
+    timer.innerHTML = `${month} месяцев ${days} дней ${hour} часов ${minute} минут ${seconds} секунд`;
   }, 1000);
 
   setTimeout(() => {
-    clearInterval(intervalSecond);
-  }, time * 1000);
+    clearInterval(interval);
+  }, new Date(2025, 0, 1).getTime());
 }
 
-console.log(timerPizza(120));
+isNewYear();
